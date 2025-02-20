819ef3ab12fe2b0eef57534871926cf7282312d2
Ondřej Novák <ondra-novak@email.cz>
2025-02-20 09:43:32 +0100

    config navrh

--- conf/conf.md
+++ conf/conf.md
@@ -1,18 +1,87 @@
+# account config
+
+- **name** - jméno účtu, pokud chybí, bere se jméno souboru bez přípony
+- **exchange** - (povinné) jméno modulu zajišťující spojení se směnárnou
+- další položky - mohou obsahovat autorizaci (key, secret, nonce, etc)
+
+account je typicky v samostatnem souboru
+
+```
+account1.ini:
+
+[account]
+name=account1
+exchange=binance
+key=329uejoidjw2d3292
+secret=djnew82jdj98dj3d2
+```
+
+
+# instrument config
+- **account** - (povinne) každý instrument je svázan s accounterm
+- další položky - exchange spravující account dostane celý config
+
+
+
+Instrumenty mohou byt definovany ve stejnem souboru jako account
+
+
+```
+account1.ini:
+
+[account]
+exchange=binance
+key=329uejoidjw2d3292
+secret=djnew82jdj98dj3d2
+
+[instrument/bitcoin]
+symbol=BTCUSDT
+
+[instrument/ethereum]
+symbol=ETHUSDT
+```
+
+pokud jsou definované v jiném souboru, musí obsahovat jméno accountu
+
+```
+[instrument/bitcoin]
+symbol=BTCUSDT
+account=account1
+```
+
+
 # strategy config
 
+konfigurace strategie je v jednom ini souboru. Každá strategie má svůj ini soubor
+
+- **instruments** - obsahuje seznam instrumentů, každý má vlastní label. Label by měl
+být unikátní, ale instrumenty se mohou opakovat
+- **parameters** - obsahuje parametry strategie
+- **table** - obsahuje název databáze (tabulky, prefix)
+
 ```
-[name::strategy]
+[strategy]
+table=string
 
-instruments=@instrument_sekce
-parameters=@params_sekce
+[instruments]
+main=account1.bitcoin
+hedge=account1.ethereum
 
-
-[instrument_sekce]
-bitcoin=account.BTCUSD
-.
-.
-.
-[params_sekce]
-param1=value1
-param2=value2
+[parameters]
+key=value
+key=value
+key=value
 ```
+
+Strategie může instrumenty definovat inline
+
+```
+[instrument/xxy]
+account=account_name
+symbol=XXYUSDT
+```
+
+Pak tento instrument se též mapuje pod svým labelem=jménem
+
+
+
--- src/lib/utils/config.h
+++ src/lib/utils/config.h
@@ -17,7 +17,6 @@ public:
 
         std::optional<std::string_view> get_string(std::string_view key) const;
         Section get_subsection(std::string_view key) const;
-
     };
 
     using Value = std::variant<std::string,  Section>;
@@ -27,12 +26,12 @@ public:
         std::string name;
         std::shared_ptr<const SectionData> _parent;
         std::map<std::string, Value, std::less<> > _kv_map;
-        std::vector<std::weak_ptr<const SectionData> > _references;
+        mutable std::vector<std::weak_ptr<const SectionData> > _references;
 
     };
 
 
-    Section find_section(std::string_view name) const {
+    Section open(std::string_view name) const {
         auto iter = _root._kv_map.find(name);
         if (iter != _root._kv_map.end()) {
             return std::get<Section>(iter->second);
@@ -41,13 +40,61 @@ public:
         }
     }
 
-    static bool set_inheritence(Section base, Section derived) {
-        if (derived->_parent) return false;
-        auto d = std::const_pointer_cast<SectionData>(derived);
-        auto b = std::const_pointer_cast<SectionData>(base);
-        b->_references.push_back(d);
-        d->_parent = b;
-        return true;
+    static std::vector<Section> get_derived_sections(Section sect) {
+        std::vector<Section> ret;
+        if (sect) {
+            ret.reserve(sect->_references.size());
+            for (const auto &c: sect->_references) {
+                Section s = c.lock();
+                if (s) ret.push_back(s);
+            }
+        }
+        return ret;
+    }
+
+
+
+    static inline std::string_view trim(std::string_view str) {
+        const size_t first = str.find_first_not_of(" \t");
+        if (first == std::string_view::npos) return {};
+        const size_t last = str.find_last_not_of(" \t");
+        return str.substr(first, last - first + 1);
+    }
+
+    template<std::invocable<std::string &> LineSource,
+             std::invocable<std::string_view, std::string_view, std::string_view> Callback >
+    static void parseIniStream(LineSource &&input, Callback &&callback) {
+        std::string line;
+        std::string currentSection;
+
+        static_assert(std::is_invocable_r_v<bool, LineSource, std::string &>,
+                "Source must return true - valid line, false - eof");
+
+        while (input(line)) {
+            std::string_view line_view = trim(line);
+
+            // Ignoruj prázdné řádky nebo komentáře
+            if (line_view.empty() || line_view.front() == '#') continue;
+
+            // Detekce sekce [sekce]
+            if (line_view.front() == '[' && line_view.back() == ']') {
+                currentSection = std::string(trim(line_view.substr(1, line_view.size() - 2)));
+            } else {
+                // Zpracuj klíč = hodnota
+                size_t eqPos = line_view.find('=');
+                if (eqPos != std::string_view::npos) {
+                    std::string_view key_view = trim(line_view.substr(0, eqPos));
+                    std::string_view value_view = trim(line_view.substr(eqPos + 1));
+                    callback(currentSection, key_view, value_view);
+                }
+            }
+        }
+    }
+
+    template<std::invocable<std::string &> LineSource>
+    void parse(LineSource &&input) {
+        std::map<std::string, std::shared_ptr<SectionData>, std::less<> > tmp_map;
+
     }
 
 
